0ea5eb9470829038ce28afec4191be60e94851d6
justtdream <alina.dauzhanova@gmail.com>
2025-02-17 00:58:00 +0500

    edit file

---
+++ lab4/m_4.py
@@ -0,0 +1,6 @@
+import math
+b = int(input("Enter the length of base: "))
+h = int(input("Enter the height of the parallelogram: "))
+area = math.prod([b, h])
+
+print(f"Area of the parallelogram: {area}")
