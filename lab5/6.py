3519e7497605db69d55190e22450473333c435c6
justtdream <alina.dauzhanova@gmail.com>
2025-02-19 12:34:10 +0500

    edit file

--- lab5/6.py
+++ lab5/6.py
@@ -1,5 +1,5 @@
 import re
 with open(r'C:\Users\WIN11\Desktop\study at uni\pp2\labs\lab5\row.txt') as file:
     txt = file.read()  
-x = (re.sub(r'[ ,.]', ':', txt))
+x = (re.sub('[ ,.]', ':', txt))
 print(x)
